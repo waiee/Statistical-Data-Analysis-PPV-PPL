7bc261dd22796ba836860ab5b43fd823467e58d1
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2022-10-03 22:34:12 +0800

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from PIL import Image
+import numpy as np
+from st_aggrid import AgGrid
 
+st.set_page_config(page_title="SDA Dashboard",
+                    page_icon=":bar_chart:",
+                    layout="wide")
+
+df = pd.read_csv('matchedPeoplePPV.csv',
+                #  skiprows= 1,
+                 nrows=10000,
+                 index_col=0,
+                 )
+# print(df)\
+st.title("SDA Dashboard")
+st.text("Created by Waiee Zainol")
+st.dataframe(df)
+
+# ----- SIDEBAR -----
